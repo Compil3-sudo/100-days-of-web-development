8d8fc3911ecf5d8def430622d8c2d920b48c2cf9
Compil3 <vallardohaeris57@gmail.com>
2024-03-08 10:47:18 +0100

    added post details

--- code/00-starting-project/routes/blog.js
+++ code/00-starting-project/routes/blog.js
@@ -8,8 +8,13 @@ router.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get("/posts", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
@@ -18,14 +23,46 @@ router.get("/new-post", async function (req, res) {
 });
 
 router.post("/posts", async function (req, res) {
-  const postData = req.body;
+  const postData = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
 
   await db.query(
     "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
-    postData
+    [postData]
   );
 
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    readableDate: posts[0].date.toLocaleDateString("de-DE", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
 });
 
 module.exports = router;
