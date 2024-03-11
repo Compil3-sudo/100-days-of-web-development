642b4f7e540143d12652193f2ece96825c84df2b
Compil3 <vallardohaeris57@gmail.com>
2024-03-11 12:51:21 +0100

    learned important basics about security

--- code/00-starting-projects/db-injection/data/database.js
+++ code/00-starting-projects/db-injection/data/database.js
@@ -1,11 +1,15 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  database: 'security',
-  user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
-})
+  host: "localhost",
+  database: "security",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+
+  // multipleStatements: true,
+  // be VERY CAREFUL WHEN USING THIS
+  // multipleSatements should be false => this can lead to SQL INJECTION
+});
 
 module.exports = pool;
--- code/00-starting-projects/db-injection/routes/discussion.js
+++ code/00-starting-projects/db-injection/routes/discussion.js
@@ -1,34 +1,45 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.redirect('/discussion');
+router.get("/", function (req, res) {
+  res.redirect("/discussion");
 });
 
-router.get('/discussion', async function (req, res) {
-  let filter = '';
+router.get("/discussion", async function (req, res) {
+  let filter = "";
 
   if (req.query.author) {
-    filter = `WHERE author = "${req.query.author}"`; 
+    // this could allow you to close the query with ; and execute a different query
+    // like DROP table...
+    // filter = `WHERE author = "${req.query.author}"`;
+
+    // use ? to enable SQL default PROTECTION
+    filter = `WHERE author = ?`;
   }
 
   const query = `SELECT * FROM comments ${filter}`;
 
   console.log(query);
 
-  const [comments] = await db.query(query);
+  // this is BAD => could allow sql injection
+  // const [comments] = await db.query(query);
 
-  res.render('discussion', { comments: comments });
+  // this lets MySql package inject query => PROTECTION
+  // also use QUESTIONMARK (see above)
+  const [comments] = await db.query(query, [req.query.author]);
+
+  res.render("discussion", { comments: comments });
 });
 
-router.post('/discussion/comment', async function (req, res) {
+router.post("/discussion/comment", async function (req, res) {
+  await db.query("INSERT INTO comments (author, text) VALUES (?)", [
+    [req.body.name, req.body.comment],
+  ]);
 
-  await db.query('INSERT INTO comments (author, text) VALUES (?)', [[req.body.name, req.body.comment]])
-
-  res.redirect('/discussion');
+  res.redirect("/discussion");
 });
 
 module.exports = router;
